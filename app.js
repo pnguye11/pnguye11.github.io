8110815766032722460cdd5d592d883c5b3a6049
Phuong Nguyen <pnguye11@gmail.com>
2017-02-04 18:05:59 -0800

    last project added

--- app.js
+++ app.js
@@ -7,26 +7,26 @@ app.config(function ($stateProvider, $urlRouterProvider) {
     .state('home', {
         url: '/home',
         controller: 'homeController as home',
-        templateUrl: 'js/partials/home.html'
+        templateUrl: 'partials/home.html'
     })
     .state('about', {
         url: '/about',
         controller: 'aboutController as about',
-        templateUrl: 'js/partials/about.html'
+        templateUrl: 'partials/about.html'
     })
     .state('projects', {
         url: '/projects',
         controller: 'projectsController as projects',
-        templateUrl: 'js/partials/projects.html'
+        templateUrl: 'partials/projects.html'
     })
     .state('people', {
         url: '/people',
         controller: 'peopleController as people',
-        templateUrl: 'js/partials/people.html'
+        templateUrl: 'partials/people.html'
     })
     .state('contact', {
         url: '/contact',
         controller: 'contactController as contact',
-        templateUrl: 'js/partials/contact.html'
+        templateUrl: 'partials/contact.html'
     })
   })
